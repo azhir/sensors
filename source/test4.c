8d4636725ffaed124f6da8545ae02888f1596157
azhir <azhir@21.110.248.10.in-addr.arpa.private.cam.ac.uk>
2016-08-30 10:04:33 +0100

    Deleted .exe and made test4

--- source/test4.c
+++ source/test4.c
@@ -5,9 +5,25 @@ int main(){
 
   FILE *jsonFile;
   json_t *object;
-  json_error *error;
+  json_error_t *error;
 
-  jsonFile = fopen("/User/azhir/sensors/testlogs/sensor.json", "r");
-  
-  object = json_loadf (jsonFile, 0, error);
+  //Opening Document and Creating Object 
 
+ jsonFile = fopen("/User/azhir/sensors/testlogs/sensor.json", "r");
+ object = json_loadf (jsonFile, 0, error);
+
+
+
+ //Searching through Object for Values
+
+ value = json_object_get (myObject, "Readings");
+
+ json_object_foreach (myObject, key, value){
+   fprintf (stdout, "found key %s\n", key);
+ }
+
+
+
+
+
+}
